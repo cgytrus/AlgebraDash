08e0faa2671c3699807507b5154efe89c4eec757
ConfiG <cgytrus@cgyt.ru>
2023-06-01 13:06:46 +0300

    this doesnt need noinline

--- src/features/improvements/force_rgba8888.cpp
+++ src/features/improvements/force_rgba8888.cpp
@@ -3,10 +3,10 @@ using namespace geode::prelude;
 #include <Geode/modify/CCTexture2D.hpp>
 
 struct ForceRGBA8888 : geode::Modify<ForceRGBA8888, CCTexture2D> {
-    GEODE_NOINLINE static void setDefaultAlphaPixelFormat(CCTexture2DPixelFormat format) {
+    static void setDefaultAlphaPixelFormat(CCTexture2DPixelFormat format) {
         CCTexture2D::setDefaultAlphaPixelFormat(kCCTexture2DPixelFormat_RGBA8888);
     }
-    GEODE_NOINLINE static CCTexture2DPixelFormat defaultAlphaPixelFormat() {
+    static CCTexture2DPixelFormat defaultAlphaPixelFormat() {
         return kCCTexture2DPixelFormat_RGBA8888;
     }
 };
