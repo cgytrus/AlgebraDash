f5b8802a9948e852cf758967214a23c26ddc4930
ConfiG <cgytrus@cgyt.ru>
2023-06-12 13:58:30 +0300

    fast alt tab

---
+++ src/features/improvements/fast_alt_tab.cpp
@@ -0,0 +1,24 @@
+#include <Geode/Geode.hpp>
+using namespace geode::prelude;
+#include <Geode/modify/AppDelegate.hpp>
+
+bool isUnfocusing = false;
+struct FastAltTab : geode::Modify<FastAltTab, AppDelegate> {
+    void applicationDidEnterBackground() {
+        isUnfocusing = true;
+        AppDelegate::applicationDidEnterBackground();
+        isUnfocusing = false;
+    }
+    void trySaveGame() {
+        if(isUnfocusing) {
+            log::debug("fast alt tab: skipping save");
+            return;
+        }
+        AppDelegate::trySaveGame();
+    }
+
+    static void onModify(auto& self) {
+        if(!self.setHookPriority("AppDelegate::trySaveGame", -9999))
+            log::warn("fast alt tab: failed to set hook priority, some stuff might still save");
+    }
+};
